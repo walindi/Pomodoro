02d0eeb2c7ff5392251460e9c193f7ade818ad5c
walindi <walindibarack@yahoo.com>
2022-10-04 09:24:03 +0300

    Added text-to-speech output

--- pomodoro.sh
+++ pomodoro.sh
@@ -10,10 +10,12 @@ do
 		sleep 30m
 
 		# Create desktop notification
-		notify-send "Time for a Short Break sir ;)"
+		notify-send "Please take a Short Break sir ;)"
+		spd-say "Please take a short break sir" # Male voice
 
 		sleep 5m
 		notify-send "Your 5 minutes is up. Let's get back to work"
+		spd-say "Your 5 minutes is up. Let's get back to work"
 	done
 
 	# Fourth pomodoro
@@ -21,7 +23,9 @@ do
 
 	# Long break after 4th pomodoro
 	notify-send "Time for a long break." "Take 15 minutes off"
+	spd-say "Done with the fourth pomodoro. You may take 15 minutes off"
 	sleep 15m
 	notify-send "Time to get back to work"
+	spd-say "Time to get back to work"
 
 done
